1d751153d57052cffae2addbacd109dccc8d1c4b
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2025-05-27 22:03:56 +0530

    Add solution for Divisible and Non-divisible Sums Difference

---
+++ LeetCode/Divisible-and-Non-divisible-Sums-Difference/Solution.py
@@ -0,0 +1,11 @@
+
+class Solution {
+  int differenceOfSums(int n, int m) {
+    int num1 =0,num2 =0;
+    for(int i = 1 ; i <= n ; i++){
+        if(i % m != 0) num1 += i;
+        else num2 +=i;
+    }
+    return num1 - num2;
+  }
+}
