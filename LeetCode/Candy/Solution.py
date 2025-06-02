a19ceff393a1bc9b878d74ae54962629fc65755d
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2025-06-02 18:47:39 +0530

    Add solution for Candy

---
+++ LeetCode/Candy/Solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n  # Step 1: Give each child 1 candy initially
+
+        # Step 2: Left to Right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Step 3: Right to Left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        # Step 4: Return total candies
+        return sum(candies)
