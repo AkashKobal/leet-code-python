b7ca2aede3371e2133e667a7b8bd677abf1475c5
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2025-07-01 13:38:42 +0530

    Add solution for Type of Triangle

---
+++ LeetCode/Type-of-Triangle/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public String triangleType(int[] nums) {
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+            return "equilateral";
+        else if ((nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2])
+                 && nums[0] + nums[1] > nums[2]
+                 && nums[1] + nums[2] > nums[0]
+                 && nums[0] + nums[2] > nums[1])
+            return "isosceles";
+        else if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]
+                 && nums[0] + nums[1] > nums[2]
+                 && nums[1] + nums[2] > nums[0]
+                 && nums[0] + nums[2] > nums[1])
+            return "scalene";
+        return "none";
+    }
+}
