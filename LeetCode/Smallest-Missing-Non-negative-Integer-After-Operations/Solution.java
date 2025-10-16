7ade42da6d1f3d12bdb7af1bc640d46506ebc79f
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2025-10-16 15:27:40 +0530

    Add solution for Smallest Missing Non-negative Integer After Operations

---
+++ LeetCode/Smallest-Missing-Non-negative-Integer-After-Operations/Solution.java
@@ -0,0 +1,4 @@
+Array: [0, 1, 2, 3] → MEX = 4 (because 0,1,2,3 are there, but 4 is missing)
+Array: [1, 2, 3] → MEX = 0 (because 0 is missing)
+Array: [0, 2, 3] → MEX = 1 (because 1 is missing)
+Array: [-5, 0, 1, 3, 10] → MEX = 2 (because 2 is the first missing non-negative number)
