e8d09f817f2649e185faa0e86c55cdca4fda63a6
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2025-12-26 11:08:06 +0530

    Add solution for Minimum Penalty for a Shop

---
+++ LeetCode/Minimum-Penalty-for-a-Shop/Solution.java
@@ -0,0 +1,12 @@
+0: nn=0 y_rem=4 penalty=4
+1: nn=1 y_rem=4 penalty=5
+2: nn=1 y_rem=3 penalty=4
+3: nn=1 y_rem=2 penalty=3
+4: nn=1 y_rem=1 penalty=2
+5: nn=2 y_rem=1 penalty=3
+6: nn=3 y_rem=1 penalty=4
+7: nn=4 y_rem=1 penalty=5
+8: nn=4 y_rem=0 penalty=4
+9: nn=5 y_rem=0 penalty=5
+10: nn=6 y_rem=0 penalty=6
+minI=4
