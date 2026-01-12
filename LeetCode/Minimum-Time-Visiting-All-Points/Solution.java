5e114c6be8268a65f1d20f5b0da1a2766c47c5d5
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2026-01-12 12:34:08 +0530

    Add solution for Minimum Time Visiting All Points

---
+++ LeetCode/Minimum-Time-Visiting-All-Points/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] p) {
+        int ans = 0;
+        for (int i = 1; i < p.length; i++) {
+            ans += Math.max(
+                Math.abs(p[i][0] - p[i - 1][0]),
+                Math.abs(p[i][1] - p[i - 1][1])
+            );
+        }
+        return ans;
+    }
+}
