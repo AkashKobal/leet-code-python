9883c8f0061500c68c3ae05a5e837c269e7d00ba
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2026-01-30 19:09:25 +0530

    Add solution for Minimum Cost to Convert String II

---
+++ LeetCode/Minimum-Cost-to-Convert-String-II/Solution.java
@@ -0,0 +1,40 @@
+"vvabsutautvsutvavtsvstabvbvtutbvuautaatb"
+"taasvsstbstasbtuauvtbtusbssbbssbuusuvbbu"
+["v","b","s","u","t","u","v","t","a","t","s","v","v","a"]
+["a","s","v","s","s","b","t","b","u","u","b","s","b","b"]
+[813,860,971,768,419,950,866,741,989,756,325,806,507,571]
+"vvvsaavtavaabtssasstbvuusbubbbsastvttbba"
+"utssutvbabttvstsbbusttuuutvvbtasvuvbvvav"
+["v","a","a","v","b","t","s","a","s","b","s","u","s","a","s","t","t","b","a"]
+["t","u","t","b","v","s","t","b","b","t","u","v","a","s","v","b","v","a","v"]
+[722,823,981,465,482,999,572,191,410,968,377,549,658,692,898,263,928,197,831]
+"asmlmoumomvummakmlbabvmvvavlavtsvbvssuumsllttsusts"
+"asmlmoumomvummakmlbabvmvvaolklllvbsltuomkslvmmusts"
+["vlavtsvbvssuumslltts","mtmubkasuvumkobbmsmo","vsbbvauauvuvsauaastl","uovumoluksslvkvlkmam","smsvsuubusskmublvvst","momuatbkubosmmavvssk","mommltotttbtvlvalsbt","vbbbuvslutblvvkvtmmo","boaosasttvtvtabtubab","mbtkumblvbasoobaauvm","vbotklaoltambktlulot","vluamsokkbaalsmmalav","mttmbuosbmlttabmbabl","sskvkbmlabaulluomovt","lstbuomkaoatmavsmvml"]
+["mtmubkasuvumkobbmsmo","vsbbvauauvuvsauaastl","uovumoluksslvkvlkmam","smsvsuubusskmublvvst","momuatbkubosmmavvssk","mommltotttbtvlvalsbt","vbbbuvslutblvvkvtmmo","boaosasttvtvtabtubab","mbtkumblvbasoobaauvm","vbotklaoltambktlulot","vluamsokkbaalsmmalav","mttmbuosbmlttabmbabl","sskvkbmlabaulluomovt","lstbuomkaoatmavsmvml","olklllvbsltuomkslvmm"]
+[9758,7133,9355,8885,6055,7360,9168,9288,7422,6995,8167,6154,6939,6343,9733]
+"joctpfyjfsw"
+"nwodttyvlpe"
+["n", "fmbgv", "xmwy", "vffn", "phfu", "say", "tqso"]
+["i", "xqgah", "kpqf", "rvvd", "llrs", "yod", "klcf"]
+[622640, 222460, 788352, 102596, 192609, 906099, 665189]
+"rpqwembtuhfycmffvgj"
+"mayotqdtvlbffvlzrfs"
+["em", "igbt", "f", "d", "vthz", "xspi", "quqi", "yax", "zqkm"]
+["hm", "gieu", "w", "s", "xdri", "mewi", "ngez", "cxz", "meqe"]
+[242857, 557385, 968458, 195333, 969065, 540239, 971546, 343745, 18206]
+"middbiab"
+"tccqryja"
+["wpoh", "ulv", "ove"]
+["jxcg", "kst", "jny"]
+[748752, 117346, 399186]
+"knnkgwxfbngx"
+"espshllvfdrk"
+["okl", "n", "kzw", "vyt", "inzqu", "h", "dod", "z"]
+["dco", "p", "gjj", "bpv", "gszyt", "b", "bgm", "r"]
+[270760, 190985, 575126, 505370, 85824, 729963, 77238, 325935]
+"ajhpd"
+"djjdc"
+["hpd","iyk","qzd","hpi","aic","znh","cea","fug","wir","kwu","yjo","rzi","a","n","f","q","u","w","x","i","x","s","o","u"]
+["iyk","qzd","hpi","aic","znh","cea","fug","wir","kwu","yjo","rzi","jdc","n","f","q","u","w","x","i","x","s","o","u","d"]
+[80257,95140,96349,89449,81714,5859,96734,96109,41211,99975,57611,32644,82896,22164,99889,98061,95403,90922,64031,94558,58418,99717,96588,88286]
