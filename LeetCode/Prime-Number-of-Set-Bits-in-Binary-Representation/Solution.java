8545d3d258ebe79629c2545c3efe6e718520605a
AKASH KOBAL <98216083+AkashKobal@users.noreply.github.com>
2026-02-21 12:12:16 +0530

    Add solution for Prime Number of Set Bits in Binary Representation

---
+++ LeetCode/Prime-Number-of-Set-Bits-in-Binary-Representation/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int count=0;
+        
+        for (int i=left;i<=right;i++) {
+            int setBits=Integer.bitCount(i);
+            if (isPrime(setBits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    private boolean isPrime(int n) {
+        if (n<=1) {
+            return false;
+        }
+        for (int i=2;i*i <= n;i++) {
+            if (n%i==0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
